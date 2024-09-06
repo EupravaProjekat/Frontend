d8b7407a9de73eec394d47607020ecc440a6617e
MihajloJankovic <101173449+MihajloJankovic@users.noreply.github.com>
2024-09-06 08:27:17 +0200

    Update config.service.ts

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@angular/core';
 export class ConfigService {
 
   _api_url: string;
+  _api_url2: string;
+
   _login_url: string;
   _check_url: string;
   _register_url: string;
@@ -30,20 +32,21 @@ export class ConfigService {
   // _border_police_request1_url: string;
 
   constructor() {
-    this._api_url = 'http://localhost:9094'; // Adjust the port as needed
+    this._api_url = 'http://localhost:9094';
+    this._api_url2 = 'http://localhost:9099';// Adjust the port as needed
     this._login_url = this._api_url + '/login';
     this._register_url = this._api_url + '/register';
     this._profile_url = this._api_url + '/profile';
-    this._profile_setup_url = this._api_url + '/profileSetup';
+    this._profile_setup_url = this._api_url2 + '/profileSetup';
     this._profile_edit_url = this._api_url + '/update-profile';
     this._getTicket_url = this._api_url + '/getTicket';
     this._activate_url = this._api_url + '/activate';
     this._reset_request_url = this._api_url + '/request-reset';
     this._reset_password_url = this._api_url + '/reset';
-    this._weapon_request_url = this._api_url + '/weapon-request';
-    this._appointments_url = this._api_url + '/appointments';
-    this._document_request_url = this._api_url + '/document-request';
-    this._parking_request_url = this._api_url + '/parking-request';
+    this._weapon_request_url = this._api_url2 + '/weapon-request';
+    this._appointments_url = this._api_url2 + '/appointments';
+    this._document_request_url = this._api_url2 + '/document-request';
+    this._parking_request_url = this._api_url2 + '/parking-request';
     this._save_border_user_url = "http://localhost:9099/adddata"
     this._check_url ="http://localhost:9099/checkifuserexists";
     this._check_border_url ="http://localhost:9098/profile";
